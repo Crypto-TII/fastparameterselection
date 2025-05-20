b69c950aada2f0d56a03ee126262306e123b3f0e
sergirovira <roviracisternasergi@gmail.com>
2025-05-20 12:11:26 +0400

    added more examples for non-fhe case

--- run_examples_non_fhe.sh
+++ run_examples_non_fhe.sh
@@ -8,3 +8,4 @@ python3 src/estimate.py --param "lambda" --n "512" --logq "13" --secret "binomia
 python3 src/estimate.py --param "n" --lambda "128" --logq "13" --secret "binomial" --error "binomial" --std "4.05" -v
 python3 src/estimate.py --param "logq" --lambda "128" --n "512" --secret "binomial" --error "binomial" --std "4.05" -v
 python3 src/estimate.py --param "std_e" --lambda "128" --n "512" --logq "13" --secret "binomial" -v
+
